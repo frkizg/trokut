904377660cfc97fec1a91b22d84d809b52b2afec
frkizg <frkizg@gmail.com>
2018-11-28 20:19:29 +0100

    Initial Commit

---
+++ trokut/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  trokut
+//
+//  Created by Zvonimir Grebenar  on 28/11/2018.
+//  Copyright © 2018 Zvonimir Grebenar . All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
